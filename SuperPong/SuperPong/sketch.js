814325e2645a9f4dd265a9650a467b7de0c31f6f
jackrabytt <39165414+jackrabytt@users.noreply.github.com>
2018-05-10 12:32:16 -0400

    Update sketch.js

--- SuperPong/SuperPong/sketch.js
+++ SuperPong/SuperPong/sketch.js
@@ -1,8 +1,10 @@
+//Declaring variable numbers, sprites, score.
 var dx = 10;
 var dy = 10;
 var spr1;
 var spr2;
 var score = 0;
+//creating canvas, paddle and ball.
 function setup() {
   createCanvas(1490, 710);
   paddle = createSprite(
@@ -12,6 +14,7 @@ function setup() {
   ball.shapeColor = (random(0, 255), random(0, 255), random(0, 255));
     
 }
+//Drawing background, defining ball movement.
 function draw() {
     background(random(0, 255), random(0, 255), random(0, 255), 20);
     ball.position.x = (ball.position.x)+ dx;
@@ -19,11 +22,11 @@ function draw() {
     textSize(72);
     
 
-
+//Making the paddle able to collide with the ball.
 paddle.velocity.y = (mouseY-paddle.position.y)*0.2;
   ball.collide(paddle, collision)
     
-    
+//Creating barriers on the top, bottom, and left walls causing the ball to collide and bounce off of it.
 if(ball.position.x > width){
 //    dx = -dx 
 //    ball.position.x = ball.position.x + dx;
@@ -44,10 +47,12 @@ if(ball.position.y < 0){
     if(ball.position.x < width){
      ball.position.x = ball.position.x + dx;
 } 
+  //Telling you if you lost. Aka the ball goes past the paddle on the right wall.
        if (ball.position.x >= width) {
            stroke(255);
            fill(255);
             text("YOU SUCK", width/2, height/2);
+  //Setting up score counter.
         }
     if(ball.position.x <= 10){
         score += 1;
@@ -59,6 +64,7 @@ if(ball.position.y < 0){
        
   drawSprites();
 }
+//Collision
 function collision (){
     dx = -dx 
     ball.position.x = ball.position.x + dx;  
